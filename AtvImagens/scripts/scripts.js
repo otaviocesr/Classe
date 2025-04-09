991a1d09a609953258bd2b408d9a23b116dae80a
OtavioCavalheiroSENAI <163847004+otaviocesr@users.noreply.github.com>
2025-04-09 12:20:21 -0300

    AtividadeClima

--- AtvImagens/scripts/scripts.js
+++ AtvImagens/scripts/scripts.js
@@ -25,6 +25,8 @@
 //         return response.json();
 //       })
 //       .then((data) => {
+// data = response.json()
+
 //         if (data.erro) {
 //           reject("CEP não foi encontrado!");
 //         } else {
@@ -45,16 +47,123 @@
 
 //   async/await
 
-async function buscarEndereco(cep) {
-  try {
-    console.log("Buscando endereço...");
-    const resposta = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
-    const dados = await resposta.json();
-    console.log("Endereço encontrado: ", dados);
-  } catch (error) {
-    console.log(error);
+// async function buscarEndereco(cep) {
+//   try {
+//     console.log("Buscando endereço...");
+//     const resposta = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+//     const dados = await resposta.json();
+//     console.log("Endereço encontrado: ", dados);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+
+// buscarEndereco("01001000");
+// console.log("Requisição enviada!");
+
+class Endereco {
+  constructor(
+    cep = "",
+    logradouro = "",
+    bairro = "",
+    localidade = "",
+    uf = "",
+    complemento = ""
+  ) {
+    this.cep = cep;
+    this.logradouro = logradouro;
+    this.bairro = bairro;
+    this.localidade = localidade;
+    this.uf = uf;
+    this.complemento = complemento;
+  }
+
+  async buscarEndereco(cep) {
+    try {
+      let response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+      let dados = await response.json();
+
+      console.log(dados);
+
+      this.cep = dados.cep;
+      this.logradouro = dados.logradouro;
+      this.bairro = dados.bairro;
+      this.localidade = dados.localidade;
+      this.uf = dados.uf;
+      this.complemento = dados.complemento || "";
+
+      return this;
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
   }
 }
 
-buscarEndereco("01001000");
-console.log("Requisição enviada!");
+class TipoEndereco extends Endereco {
+  constructor(cep, logradouro, bairro, localidade, uf, complemento, tipo) {
+    super(cep, logradouro, bairro, localidade, uf, complemento);
+    this.tipo = tipo;
+  }
+
+  exibirEndereco() {
+    return {
+      tipo: this.tipo,
+      cep: this.cep,
+      logradouro: this.logradouro,
+      bairro: this.bairro,
+      localidade: this.localidade,
+      uf: this.uf,
+      complemento: this.complemento,
+    };
+  }
+}
+
+document
+  .getElementById("cep")
+  .addEventListener("input", async function (event) {
+    const cep = event.target.value.replace(/\D/g, "");
+
+    if (cep.length === 8) {
+      const endereco = new Endereco();
+      const results = await endereco.buscarEndereco(cep);
+
+      if (results) {
+        document.getElementById("logradouro").value = results.logradouro;
+        document.getElementById("bairro").value = results.bairro;
+        document.getElementById("localidade").value = results.localidade;
+        document.getElementById("uf").value = results.uf;
+        document.getElementById("complemento").value =
+          results.complemento || "";
+      } else {
+        alert("CEP INVÁLIDO!");
+      }
+    }
+  });
+
+document.querySelector("form").addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const cep = document.getElementById("cep").value;
+  const logradouro = document.getElementById("logradouro").value;
+  const bairro = document.getElementById("bairro").value;
+  const localidade = document.getElementById("localidade").value;
+  const uf = document.getElementById("uf").value;
+  const complemento = document.getElementById("complemento").value;
+  const tipo = document.getElementById("tipo_endereco").value;
+
+  const enderecoCompleto = new TipoEndereco(
+    cep,
+    logradouro,
+    bairro,
+    localidade,
+    uf,
+    complemento,
+    tipo
+  );
+
+  console.log(
+    "Endereço cadastrado com sucesso! \n" +
+      JSON.stringify(enderecoCompleto.exibirEndereco(), null, 2)
+  );
+});
